79affec94232f6173a830b4ba9e58214bc827823
nmthanh206 <nmthanh20006@gmail.com>
2023-06-02 02:21:35 +0700

    fix item

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const championsData = require("./champion.json").data;
 const itemsData = require("./items.json").data;
 const fs = require("fs");
 const run = async () => {
-   //api champions http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion.json
-   //api item champion http://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/Aatrox.png
+   //api champions http://ddragon.leagueoflegends.com/cdn/13.11.1/data/en_US/champion.json
+   //api item champion http://ddragon.leagueoflegends.com/cdn/13.11.1/img/champion/Aatrox.png
    await fs.promises.writeFile(
       "convert-champion.json",
       JSON.stringify(
@@ -17,22 +17,26 @@ const run = async () => {
             ]) => {
                return {
                   championName,
-                  image: `http://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/${full}`,
+                  image: `http://ddragon.leagueoflegends.com/cdn/13.11.1/img/champion/${full}`,
                   id: key,
                };
             }
          )
       )
    );
-   //api items http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/item.json
-   //api item image http://ddragon.leagueoflegends.com/cdn/13.10.1/img/item/1001.png
+   //api items http://ddragon.leagueoflegends.com/cdn/13.11.1/data/en_US/item.json
+   //api item image http://ddragon.leagueoflegends.com/cdn/13.11.1/img/item/1001.png
    await fs.promises.writeFile(
       "convert-items.json",
       JSON.stringify(
          Object.entries(itemsData)
             .filter(
-               ([_, { maps, into, from }]) =>
-                  maps["12"] && !into?.length && from?.length
+               ([_, { maps, into, from, depth }]) =>
+                  (depth > 2 && maps["12"] && !into?.length && from?.length) ||
+                  (maps["12"] &&
+                     !into?.length &&
+                     from?.length &&
+                     from.includes("1001"))
             )
             .map(([imageCode, { description, name, from }]) => {
                const isBoots = (from && from.includes("1001")) || false;
@@ -42,7 +46,7 @@ const run = async () => {
                   ) || false;
                return {
                   itemName: name,
-                  image: `https://ddragon.leagueoflegends.com/cdn/13.10.1/img/item/${imageCode}.png`,
+                  image: `https://ddragon.leagueoflegends.com/cdn/13.11.1/img/item/${imageCode}.png`,
                   isBoots,
                   isLegendary,
                   id: imageCode,
